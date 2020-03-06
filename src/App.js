ddd4e8f135279e5530cf92968e666b3e42e241db
David Nielsen <davidnielsen215@gmail.com>
2020-03-05 17:01:30 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [inputData, setInputData] = useState({firstName:'', lastName: ''})
+  const [contact, setContact] = useState([])
+
+  function handleChange(e){
+    const {name, value} = e.target
+    setInputData(prevState => ({...prevState, [name]: value}))
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setContact(prevContact => [...prevContact, inputData])
+  }
+
+  const addContact = contact.map(contact => <h3>{contact.firstName} {contact.lastName}</h3>)
+  
+  console.log(inputData)
+  console.log(contact)
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+        placeholder="first name"
+        name="firstName"
+        value={inputData.firstName}
+        onChange={handleChange}/>
+        <input
+        placeholder="last name"
+        name="lastName"
+        value={inputData.lastName}
+        onChange={handleChange}/>
+        <button>add contact</button>
+      </form>
+      {addContact}
     </div>
-  );
+  )
 }
-
-export default App;
